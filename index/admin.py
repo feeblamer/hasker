48a28fb8f71e0da8a2ac8ce5d64819ddddcc385d
Alexandr Maslyakov <adecea@gmail.com>
2021-08-29 19:18:54 +0500

    Изменена модель Answer
    Добавлена связь с моделью Question.

--- index/admin.py
+++ index/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from index.models import User, Question, Answer
+
+admin.site.register(User)
+admin.site.register(Question)
+admin.site.register(Answer)
+
--- index/models.py
+++ index/models.py
@@ -13,18 +13,43 @@ class User(models.Model):
     def __str__(self):
         return self.nickname
 
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ['reg_date', 'nickname', 'email']
+
 class Tag(models.Model):
     word = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.word
 
 class Question(models.Model):
     title = models.CharField(max_length=250)
     content = models.TextField()
-    author = models.ForeignKey(User , on_delete=models.CASCADE,)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING,)
     create_date = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField(Tag)
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Вопрос"
+        verbose_name_plural = "Вопросы"
+        ordering = ['create_date']
+
 class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     write_date = models.DateTimeField(auto_now_add=True)
     is_correct = models.BooleanField()
+    
+    def __str__(self):
+        return self.question.title + self.content
+
+    class Meta:
+        verbose_name = "Ответ"
+        verbose_name_plural = "Ответы"
+        ordering = ['write_date']
